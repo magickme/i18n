362f5512279b3f15e736a07cce864f561f2942b1
Jason Louv <jason@ultraculture.org>
2023-09-23 15:25:41 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+    const src = "https://media.tate.org.uk/aztate-prd-ew-dg-wgtail-st1-ctr-data/images/blakejerusalemtitlepage.width-840.jpg";
+</script>
+
+<article class="centered">
+
+    <img {src} alt="William Blake, Jerusalem" height="600px"/>
+
+    <h1>Jerusalem</h1>
+    <h2>William Blake</h2>
+
+    <p>And did those feet in ancient time<br/>
+    Walk upon Englands mountains green:<br/>
+    And was the holy Lamb of God,<br/>
+    On Englands pleasant pastures seen!</p>
+    
+    <p>And did the Countenance Divine,<br/>
+    Shine forth upon our clouded hills?<br/>
+    And was Jerusalem builded here,<br/>
+    Among these dark Satanic Mills?</p>
+    
+    <p>Bring me my Bow of burning gold:<br/>
+    Bring me my arrows of desire:<br/>
+    Bring me my Spear: O clouds unfold!<br/>
+    Bring me my Chariot of fire!</p>
+    
+    <p>I will not cease from Mental Fight,<br/>
+    Nor shall my sword sleep in my hand:<br/>
+    Till we have built Jerusalem,<br/>
+    In Englands green & pleasant Land.</p>
+
+        <br /><br />
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/C79L3vjKAWQ?si=HNF2J-JciYelOoUR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+</article>
+
+<style>
+    h1 {
+        font-size: 3em;
+    }
+    h2 {
+        font-size: 2em;
+    }
+    p {
+        font-size: 1.5em;
+    }
+    .centered {
+        text-align:center;
+        margin: 30px 0px 80px 0px;
+    }
+</style>
